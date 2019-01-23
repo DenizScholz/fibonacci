a1e714339cc2680252e1c14029f9ab0ca3e95829
Deniz <deniz.scholz@t-online.de>
2019-01-23 23:21:38 +0100

    idk

--- index.js
+++ index.js
@@ -1,15 +1,37 @@
 function calcFibonacci (n) {
+
+  /*
+
+  this is recursive implementation --> bad!
+
   if (n == 1 || n == 2) {
     return 1;
   } else {
     return calcFibonacci(n-1) + calcFibonacci(n-2);
   }
+  */
+
+  if (n == 1 || n == 2) {
+    return 1;
+  } else {
+    var pre = 1;
+    var prepre = 1;
+    actual = 2;
+    for (i = 3; i < n; i++) {
+      prepre = pre;
+      pre = actual;
+      actual = pre + prepre;
+    }
+    return actual;
+  }
 }
 
 function initFibo() {
   var n = parseInt(document.getElementById("number").value);
+  document.getElementById("number").value = "";
   if (!n) {
     alert("Please make sure you enter a number greater than 0!");
+    return;
   }
   var startDate = new Date();
 
@@ -18,7 +40,7 @@ function initFibo() {
   var endDate   = new Date();
   var seconds = (endDate.getTime() - startDate.getTime()) / 1000;
 
-  document.getElementById("timetaken").innerHTML = "It took " + seconds + "s to calculate!";
+  document.getElementById("timetaken").innerHTML = "It took " + seconds + " seconds to calculate!";
 
   document.getElementById("result").style.padding = "20px";
   document.getElementById("timetaken").style.padding = "20px";
