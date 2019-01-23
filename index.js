27d0ca4104938bc12ea97ba196c7e95bed94d150
Deniz <deniz.scholz@t-online.de>
2019-01-23 22:32:55 +0100

    lol

--- index.js
+++ index.js
@@ -4,13 +4,23 @@ function calcFibonacci (n) {
   } else {
     return calcFibonacci(n-1) + calcFibonacci(n-2);
   }
-
-  console.log(result)
-
 }
 
 function initFibo() {
   var n = parseInt(document.getElementById("number").value);
-  document.getElementById("result").innerHTML = "F(" + n + ") = " + calcFibonacci(n);
+  if (!n) {
+    alert("Please make sure you enter a number greater than 0!");
+  }
+  var startDate = new Date();
 
+  document.getElementById("result").innerHTML = "Fibonacci(" + n + ") = " + calcFibonacci(n);
+
+  var endDate   = new Date();
+  var seconds = (endDate.getTime() - startDate.getTime()) / 1000;
+
+  document.getElementById("timetaken").innerHTML = "It took " + seconds + "s to calculate!";
+
+  document.getElementById("result").style.padding = "20px";
+  document.getElementById("timetaken").style.padding = "20px";
+  document.getElementById("timetaken").style.color = "rgb(255,255,255,0.5)";
 }
